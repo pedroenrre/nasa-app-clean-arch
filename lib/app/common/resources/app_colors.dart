d94d3e2dc8e1841c48bc4865dac89b374dfc8ce1
pedroenrre <pedro_enrre@hotmail.com>
2022-04-30 12:21:36 -0300

    adds initial resources

---
+++ lib/app/common/resources/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff0b3d91);
+  static const Color secondary = Color(0xfffc3d21);
+  static const Color background = Color(0xffFBFBFB);
+  static const Color grey = Color(0xffDDDDDD);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff000000);
+}
